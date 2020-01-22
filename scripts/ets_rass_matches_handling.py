1a50c81d9438db6195891a7bd88177ace7141706
ggarcia01 <44787764+ggarcia01@users.noreply.github.com>
2020-01-22 14:34:32 -0500

    adding ETS offsets

--- scripts/ets_rass_matches_handling.py
+++ scripts/ets_rass_matches_handling.py
@@ -14,6 +14,25 @@ df['usrid'] = usrid_lst
 restriction1 = df['normalized_offset'] < 1.4
 df = df[restriction1]
 size_after = df.shape
+
+def ets_offsets_from_snr(dataframe,snr_column):
+    offset = []
+    for i in range(dataframe.shape[0]):
+        snr = dataframe[snr_column].iloc[i]
+        if snr <=3:
+            offset += [73]
+        elif snr > 3 and snr <=4:
+            offset +=[53]
+        elif snr >4 and snr <=6:
+            offset +=[41]
+        elif snr > 6:
+            offset +=[30]
+    dataframe['ets_offset'] = offset
+    return dataframe
+
+df = ets_offsets_from_snr(df,'ets_snr')
+
+
 #overwrite the data file:
 df.to_csv('../ets_rass_matches_files/match_theta_copy.csv',index=False)
 
